f44404f3ff80f476cecadac8f95c19cc4164430c
Roman Chak <chakr1@yahoo.com>
2025-04-22 16:16:00 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,4 @@
 //*****************************************************************************
-// Exosite Modifications:
-// Copyright (C) 2014 Exosite LLC
-//
-// Original File:
-// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
-// 
-// 
-//  Redistribution and use in source and binary forms, with or without 
-//  modification, are permitted provided that the following conditions 
-//  are met:
-//
-//    Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer.
-//
-//    Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the 
-//    documentation and/or other materials provided with the   
-//    distribution.
-//
-//    Neither the name of Texas Instruments Incorporated nor the names of
-//    its contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//*****************************************************************************
-
-//*****************************************************************************
 //
 // Application Name     - Exosite Cloud Demo
 // Application Overview - This Application demonstrates using the CC3200
@@ -44,12 +6,6 @@
 //
 //*****************************************************************************
 
-//****************************************************************************
-//
-//! \addtogroup Exosite
-//! @{
-//
-//****************************************************************************
 
 // Standard includes
 #include <stdlib.h>
@@ -123,9 +79,9 @@ typedef enum
   LED_BLINK
 }eLEDStatus;
 
-// Application specific status/error codes
+//Application-specific status/error codes
 typedef enum{
-    // Choosing -0x7D0 to avoid overlap w/ host-driver's error codes
+    // Choosing -0x7D0 to avoid overlap with/ host-driver's error codes
     LAN_CONNECTION_FAILED = -0x7D0,
     INTERNET_CONNECTION_FAILED = LAN_CONNECTION_FAILED - 1,
     DEVICE_NOT_IN_STATION_MODE = INTERNET_CONNECTION_FAILED - 1,
@@ -185,7 +141,7 @@ extern uVectorEntry __vector_table;
 #ifdef USE_FREERTOS
 //*****************************************************************************
 //
-//! Application defined hook (or callback) function - the tick hook.
+//! The application-defined hook (or callback) function is the tick hook.
 //! The tick interrupt can optionally call this
 //!
 //! \param  none
@@ -200,7 +156,7 @@ vApplicationTickHook( void )
 
 //*****************************************************************************
 //
-//! Application defined hook (or callback) function - assert
+//! Application-defined hook (or callback) function - assert
 //!
 //! \param  none
 //!
@@ -218,7 +174,7 @@ vAssertCalled( const char *pcFile, unsigned long ulLine )
 
 //*****************************************************************************
 //
-//! Application defined idle task hook
+//! Application-defined idle task hook
 //!
 //! \param  none
 //!
@@ -233,7 +189,7 @@ vApplicationIdleHook( void )
 
 //*****************************************************************************
 //
-//! Application provided stack overflow hook function.
+//! The application provided a stack overflow hook function.
 //!
 //! \param  handle of the offending task
 //! \param  name  of the offending task
