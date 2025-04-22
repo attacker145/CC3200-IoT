fb31e69f8efb241244bdc324a28228da85c3085b
Roman Chak <chakr1@yahoo.com>
2025-04-22 16:17:06 -0700

    Update serial_wifi.c

--- serial_wifi.c
+++ serial_wifi.c
@@ -1,16 +1,3 @@
-//*****************************************************************************
-// Copyright (C) 2014 Texas Instruments Incorporated
-//
-// All rights reserved. Property of Texas Instruments Incorporated.
-// Restricted rights to use, duplicate or disclose this code are
-// granted through contract.
-// The program may not be used without the written permission of
-// Texas Instruments Incorporated or against the terms and conditions
-// stipulated in the agreement under which this program has been supplied,
-// and under no circumstances can it be used with non-TI connectivity device.
-//
-//*****************************************************************************
-
 // Simplelink includes
 #include "simplelink.h"
 
